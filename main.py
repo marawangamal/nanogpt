Bugfix: causal mask

main.py
@@ -18,40 +18,65 @@ if __name__ == "__main__":
     n_layers = 2
     d_model = 512
     # hparams (opt)
-    epochs = 10
-    batch_size = 3
-    lr = 1e-3
+    epochs = 100
+    batch_size = 2
+    lr = 1e-2
     device = "cuda" if torch.cuda.is_available() else "cpu"
+    # hparams (data)
+    max_num_samples = 2
+
+    # reproducibility
+    torch.manual_seed(0)
 
     # load model & tokenizer
-    tokenizer = AutoTokenizer.from_pretrained("gpt2")
+    tokenizer = AutoTokenizer.from_pretrained("gpt2")  # (~30k tokens)
     model = NanoGPT(n_layers=n_layers, d_model=d_model, d_vocab=len(tokenizer))
     opt = torch.optim.AdamW(model.parameters(), lr=lr)
 
     # load data
-    stemp = STemp(tokenizer, max_num_samples=100)
+    stemp = STemp(tokenizer, max_num_samples=max_num_samples)
     stemp_ds = stemp.load_data()
     train_dl = DataLoader(stemp_ds["train"], batch_size=batch_size, collate_fn=collate_fn)  # type: ignore
     eval_dl = DataLoader(stemp_ds["eval"], batch_size=batch_size, collate_fn=collate_fn)  # type: ignore
 
     # train loop
-    for i_epoch in tqdm(range(epochs), desc="Epochs"):
-        pbar = tqdm(train_dl, desc=f"Epoch {i_epoch+1}", leave=False)
-        for batch in pbar:
+    pbar = tqdm(range(epochs), desc="Epochs")
+    for i_epoch in pbar:
+        pbar_i = tqdm(train_dl, desc=f"Epoch {i_epoch+1}", leave=False)
+        train_losses = []
+        for batch in pbar_i:
             batch = {k: v.to(device) for k, v in batch.items()}
             x = batch["input_ids"][:, :-1]
             y = batch["input_ids"][:, 1:]
             loss = model(x, y).loss
             loss.backward()
+            train_losses.append(loss.item())
             opt.step()
             opt.zero_grad()
 
             # Update progress bar description with loss
-            pbar.set_postfix(loss=f"{loss.item():.4f}")
+            pbar_i.set_postfix(loss=f"{loss.item():.4f}")
 
-        # qualitative eval
-        x_str = stemp.get_sample_prompt()
-        x = torch.tensor(tokenizer(x_str)["input_ids"]).reshape(1, -1)
-        y = model.generate(x, max_output_tokens=32)
-        y_str = tokenizer.decode(y[0].tolist())
-        print(f"[{i_epoch}/{epochs}]{x_str}::{y_str}")
+        # eval
+        with torch.no_grad():
+            # qualitative
+            x_str = stemp.get_sample_prompt()
+            x = torch.tensor(tokenizer(x_str)["input_ids"]).reshape(1, -1)
+            y = model.generate(x, max_output_tokens=32)
+            y_str = tokenizer.decode(y[0].tolist())
+            print(f"[{i_epoch}/{epochs}]Eval generation: {x_str}::{y_str}")
+
+            # quantitative
+            eval_losses = []
+            for batch in tqdm(eval_dl, desc=f"Running eval", leave=False):
+                batch = {k: v.to(device) for k, v in batch.items()}
+                x = batch["input_ids"][:, :-1]
+                y = batch["input_ids"][:, 1:]
+                loss = model(x, y).loss
+                eval_losses = eval_losses + [loss]
+
+        # update progress bar
+        pbar.set_postfix(
+            eval_loss=f"{torch.tensor(eval_losses).mean():.4f}",
+            train_loss=f"{torch.tensor(train_losses).mean():.4f}",
+        )

nanogpt/modelling_nanogpt.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List, Optional
+from typing import Optional
 import torch
 
 # TODO:
@@ -7,6 +7,13 @@ import torch
 # [ ]: Is there something more special to do for weight tieing enc/dec?
 # [ ]: add positional encoding
 # [ ]: use `stop_token` in generate
+
+# Fixes:
+# fix the mask -inf
+# pos enc
+# multi-head instead of single head
+# 4x larger in the MLP first layer
+# separate decoder
 
 
 @dataclass
@@ -20,8 +27,10 @@ class MLP(torch.nn.Module):
         super().__init__()
         self.fc = torch.nn.Sequential(
             torch.nn.Linear(d_in, d_in),
+            # torch.nn.Linear(d_in,4*d_in),
             torch.nn.ReLU(),
             torch.nn.Linear(d_in, d_in),
+            # torch.nn.Linear(4*d_in,d_in),
             torch.nn.Dropout(dropout),
         )
 
@@ -92,8 +101,13 @@ class MultiHeadAttention(torch.nn.Module):
         # compute attn matrix O(T^2D)
         gamma = 1 / torch.sqrt(torch.tensor(self.d_model))
         mask = torch.tril(torch.ones(x.size(1), x.size(1))) if not mask else mask
+        # replacements 0 => -inf, 1 => 0
+        mask[mask == 0] = -torch.inf
+        mask[mask == 1] = 0
+        # att.masked_fill(self.bias[:,:,:T,:T] == 0, float('-inf'))
+        # BUG: mask 0 should be -inf
         a = torch.softmax(
-            ((torch.einsum("bqd,bkd->bqk", q, k) * gamma) * mask),
+            ((torch.einsum("bqd,bkd->bqk", q, k) * gamma) + mask),
             dim=-1,
         )  # i.e., a[b, i, j] = <q_bi, k_bj>
 
@@ -149,7 +163,7 @@ class NanoGPT(torch.nn.Module):
         # train mode
         if y is not None:
             loss = torch.nn.functional.cross_entropy(
-                logits.reshape(-1, self.d_vocab), y.reshape(-1)
+                logits.reshape(-1, self.d_vocab), y.reshape(-1)  # (BT, V), (BT,)
             )
             return ModelOutput(logits=logits, loss=loss)
         return ModelOutput(logits=logits)