2fdc3df7acd0213b6f8c790692b89835c87d0641
Marawan Gamal <marawan.gamal@live.com>
2025-07-10 22:52:12 -0400

    Add kv cache

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from tqdm import tqdm
 
 import torch
@@ -11,6 +12,22 @@ from nanogpt.modelling_nanogpt import NanoGPT
 def collate_fn(examples):
     keys = examples[0].keys()
     return {k: torch.stack([torch.tensor(ex[k]) for ex in examples]) for k in keys}
+
+
+def eval_generation_speed(
+    model: NanoGPT, input_seq_len=32, output_seq_len=32, n_iters=10
+):
+    x = torch.randint(0, model.d_vocab, (1, input_seq_len))
+    for _ in range(3):
+        model.generate(x, max_output_tokens=output_seq_len)
+
+    start = time.perf_counter()
+    for _ in range(n_iters):
+        model.generate(x, max_output_tokens=output_seq_len)
+    if torch.cuda.is_available():
+        torch.cuda.synchronize()
+    elapsed = time.perf_counter() - start
+    return elapsed / n_iters
 
 
 if __name__ == "__main__":
@@ -40,9 +57,17 @@ if __name__ == "__main__":
     stemp_ds = stemp.load_data()
     train_dl = DataLoader(stemp_ds["train"], batch_size=batch_size, collate_fn=collate_fn)  # type: ignore
     eval_dl = DataLoader(stemp_ds["eval"], batch_size=batch_size, collate_fn=collate_fn)  # type: ignore
+    test_dl = DataLoader(stemp_ds["test"], batch_size=1, collate_fn=collate_fn)  # type: ignore
+
+    # model perf
+    print("Model Summary")
+    print(f"Params:    {sum([p.numel() for p in model.parameters()]):.2f}")
+    print(f"Size:      {sum([p.numel() * 4/1e-9 for p in model.parameters()]):.2f} GB")
+    print(f"Generate:  {eval_generation_speed(model)}s")
 
     # train loop
     pbar = tqdm(range(epochs), desc="Epochs")
+    corr, n_trials = 0, 0
     for i_epoch in pbar:
         model.train()
         pbar_i = tqdm(train_dl, desc=f"Epoch {i_epoch+1}", leave=False)
@@ -63,14 +88,16 @@ if __name__ == "__main__":
         # eval
         model.eval()
         with torch.no_grad():
-            # qualitative
+            # eval (qualitative)
             x_str = stemp.get_sample_prompt()
             x = torch.tensor(tokenizer(x_str)["input_ids"]).reshape(1, -1)
-            y = model.generate(x, max_output_tokens=32)
-            y_str = tokenizer.decode(y[0].tolist())
+            y = model.generate(
+                x, max_output_tokens=128, stop_token=tokenizer.eos_token_id
+            )
+            y_str = tokenizer.decode(y[0].tolist(), skip_special_tokens=True)
             print(f"[{i_epoch}/{epochs}]Eval generation: {x_str}::{y_str}")
 
-            # quantitative
+            # eval (quantitative)
             eval_losses = []
             for batch in tqdm(eval_dl, desc=f"Running eval", leave=False):
                 batch = {k: v.to(device) for k, v in batch.items()}
@@ -79,8 +106,25 @@ if __name__ == "__main__":
                 loss = model(x, y).loss
                 eval_losses = eval_losses + [loss]
 
+            # test
+            for batch in tqdm(test_dl, desc=f"Running test", leave=False):
+                batch = {k: v.to(device) for k, v in batch.items()}
+                x = batch["input_ids"]
+                y = batch["labels"]
+                y_hat = model.generate(
+                    x, max_output_tokens=128, stop_token=tokenizer.eos_token_id
+                )
+                y_hat = [stemp.parse_answer(p) for p in tokenizer.batch_decode(y_hat)]
+                corr += sum(y_hat[k] == y[k] for k in range(len(y_hat)))
+                n_trials += len(y_hat)
+
         # update progress bar
         pbar.set_postfix(
             eval_loss=f"{torch.tensor(eval_losses).mean():.4f}",
+            eval_acc=f"{corr/n_trials:.4f}",
             train_loss=f"{torch.tensor(train_losses).mean():.4f}",
         )
+
+    if n_trials:
+        print("Accuracy:")
+        print(f"{corr / n_trials}:<12")
